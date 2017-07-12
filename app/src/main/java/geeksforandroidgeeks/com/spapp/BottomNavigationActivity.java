d618d0af823e9d4690303de30bfec1b2bf19ff38
Vivek Samele <vivek.samele@dreamorbit.com>
2017-07-12 15:27:28 +0530

    inital comit

---
+++ app/src/main/java/geeksforandroidgeeks/com/spapp/BottomNavigationActivity.java
@@ -0,0 +1,22 @@
+package geeksforandroidgeeks.com.spapp;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class BottomNavigationActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_navigation);
+
+
+    }
+
+}
